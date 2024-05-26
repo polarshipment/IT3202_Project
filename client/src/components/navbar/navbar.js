5e7970a19bbbb71e864b146a4be74380692dc7b6
Nethu <21103670@usc.edu.ph>
2024-05-26 20:31:42 +0800

    layout, dashboard, login

--- client/src/components/navbar/navbar.js
+++ client/src/components/navbar/navbar.js
@@ -2,7 +2,14 @@ import React from 'react'
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <nav className="w-screen flex flex-row justify-between items-center bg-white px-5 py-4 border-b-[5px] border-custom-grey2">
+      <h1 className="font-['Open_Sans'] font-bold text-2xl">ONLINE SARI-SARI</h1>
+      <div className="flex items-center font-['Barlow_Semi_Condensed'] text-xl">
+        <div className="cursor-pointer ms-8">DASHBOARD</div>
+        <div className="cursor-pointer ms-8">PRODUCTS</div>
+        <div className="cursor-pointer border border-black px-4 py-1 ms-8">LOGOUT</div>
+      </div>
+    </nav>
   )
 }
 
---
+++ client/src/pages/dashboard/Dashboard.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Dashboard() {
+  return (
+    <div className='h-full w-full py-10 flex justify-center'>
+      <div className="w-8/12 h-full bg-white p-5 flex flex-col">
+        <div className="border-b-[1px] border-custom-grey2 py-1 pb-5">
+          Welcome Test!
+        </div>
+        <div className="h-full flex justify-center items-center bg-red-200">
+          <div className="">
+            
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Dashboard
