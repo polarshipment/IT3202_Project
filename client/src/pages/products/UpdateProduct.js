131ba8dca310b0cb6395495b06cab8ae9ef0c43f
chrstysrss <21102824@usc.edu.ph>
2024-05-27 14:16:00 +0800

    Update UpdateProduct.js

--- client/src/pages/products/UpdateProduct.js
+++ client/src/pages/products/UpdateProduct.js
@@ -20,6 +20,10 @@ function UpdateProduct() {
       }).catch(err => console.log(err));
     }
 
+    function handleCancel() {
+      navigate('/');
+    }
+
     return (
       <div className="bg-custom-grey1 h-full w-full flex justify-center items-center">
         <div className="bg-white min-w-[35%] py-10 px-20 rounded-md shadow-md"> 
@@ -44,6 +48,7 @@ function UpdateProduct() {
               />
             </div>
             <div style={{ textAlign: 'center' }}>
+            <button className="bg-custom-red text-white px-10 py-2 rounded-md mr-2" onClick={handleCancel} >CANCEL</button>
             <button className="bg-custom-skyblue text-white px-10 py-2 rounded-md">UPDATE</button>
             </div>
           </form>
